92c6b0382daf3c16d2f7db3b7cae61da74310d97
nol4lej <nome.ahumada@outlook.com>
2024-05-20 12:23:20 -0400

    Manejo de error en caso de que no hayan datos que renderizar

--- src/components/SubmittedData.jsx
+++ src/components/SubmittedData.jsx
@@ -1,23 +1,35 @@
 import { Box, Typography, Paper } from '@mui/material';
+import ErrorIcon from '@mui/icons-material/Error';
 
-const SubmittedData = ({ submittedData }) => (
-    <Box sx={{ mt: 3 }}>
-        <Typography variant="h6">Datos del Formulario:</Typography>
-        <Paper variant="outlined" sx={{ p: 2, overflowWrap: 'break-word' }}>
-            {Object.entries(submittedData).map(([key, value]) => (
-                value && (
-                    <Box key={key}>
-                        <Typography variant="subtitle1" component="span" fontWeight="bold">
-                            {key}:
-                        </Typography>{" "}
-                        <Typography variant="body2" component="span">
-                            {value}
-                        </Typography>
-                    </Box>
-                )
-            ))}
-        </Paper>
-    </Box>
-);
+const SubmittedData = ({ submittedData }) => {
+    if (Object.keys(submittedData).length === 0) {
+        return (
+            <Typography color="error" align="center" display="flex" alignItems="center" justifyContent="center" gap={1} padding={2}>
+                <ErrorIcon />
+                No hay datos disponibles
+            </Typography>
+        );
+    }
+
+    return (
+        <Box sx={{ mt: 3 }}>
+            <Typography variant="h6">Datos del Formulario:</Typography>
+            <Paper variant="outlined" sx={{ p: 2, overflowWrap: 'break-word' }}>
+                {Object.entries(submittedData).map(([key, value]) => (
+                    value && (
+                        <Box key={key}>
+                            <Typography variant="subtitle1" component="span" fontWeight="bold">
+                                {key}:
+                            </Typography>{" "}
+                            <Typography variant="body2" component="span">
+                                {value}
+                            </Typography>
+                        </Box>
+                    )
+                ))}
+            </Paper>
+        </Box>
+    )
+}
 
 export default SubmittedData;
