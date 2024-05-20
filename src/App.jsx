eb798d33a985d76eb6521625af5099d976f41596
nol4lej <nome.ahumada@outlook.com>
2024-05-20 11:46:21 -0400

    Contenedor principal de la aplicacion + componente principal que incializa los formularios dinamicos

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import './App.css'
+import { Container } from '@mui/material'
+import DynamicForm from './components/DynamicForm'
 
 function App() {
-
   return (
     <>
+      <Container maxWidth="sm" sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }} >
+        <DynamicForm />
+      </Container>
     </>
   )
 }
