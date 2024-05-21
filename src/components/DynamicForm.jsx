3ce8ef8c4c9d02763db90e3e9c3f1c0100fbfe0e
nol4lej <nome.ahumada@outlook.com>
2024-05-21 16:48:56 -0400

    Datos del formulario renderizado desde Modal

--- src/components/DynamicForm.jsx
+++ src/components/DynamicForm.jsx
@@ -4,11 +4,13 @@ import ErrorIcon from '@mui/icons-material/Error';
 import useDynamicFormFetch from '../hooks/useDynamicFormFetch';
 import FormField from './FormField';
 import SubmittedData from './SubmittedData';
+import Modal from './Modal';
 
 const DynamicForm = () => {
     const { formFields, isLoading, error } = useDynamicFormFetch();
     const [formData, setFormData] = useState({});
     const [submittedData, setSubmittedData] = useState(null);
+    const [openModal, setOpenModal] = useState(false);
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -30,38 +32,45 @@ const DynamicForm = () => {
         });
 
         setSubmittedData(submittedDataWithLabels);
+        setOpenModal(true)
     };
 
     return (
-        <Box component="form" sx={{ my: 3, bgcolor: '#f5f5f5', p: 4, borderRadius: 8, boxShadow: 3, width: '100%' }}>
-            <Typography variant="h4" component="h1" align="center" gutterBottom sx={{ color: '#333' }}>
-                Formulario Dinámico
-            </Typography>
-
-            {isLoading && 
-                <Box display="flex" alignItems="center" justifyContent="center" padding={2}>
-                    <CircularProgress />
-                </Box>
-            }
-            {error && 
-                <Typography color="error" align="center" display="flex" alignItems="center" justifyContent="center" gap={1} padding={2}>
-                    <ErrorIcon />
-                    {error}
+        <>
+            <Box component="form" sx={{ my: 3, bgcolor: '#f5f5f5', p: 4, borderRadius: 8, boxShadow: 3, width: '100%' }}>
+                <Typography variant="h4" component="h1" align="center" gutterBottom sx={{ color: '#333' }}>
+                    Formulario Dinámico
                 </Typography>
-            }
 
-            {!isLoading && !error && (
-                <>
-                    {formFields.map((field, index) => (
-                        <FormField key={index} field={field} index={index} formData={formData} handleChange={handleChange} />
-                    ))}
-                    <Button variant="contained" color="primary" onClick={handleSubmit} sx={{ mt: 2, ml: 'auto', display: 'block' }}>
-                        Confirmar
-                    </Button>
-                    {submittedData && <SubmittedData submittedData={submittedData} />}
-                </>
-            )}
-        </Box>
+                {isLoading && 
+                    <Box display="flex" alignItems="center" justifyContent="center" padding={2}>
+                        <CircularProgress />
+                    </Box>
+                }
+                {error && 
+                    <Typography color="error" align="center" display="flex" alignItems="center" justifyContent="center" gap={1} padding={2}>
+                        <ErrorIcon />
+                        {error}
+                    </Typography>
+                }
+
+                {!isLoading && !error && (
+                    <>
+                        {formFields.map((field, index) => (
+                            <FormField key={index} field={field} index={index} formData={formData} handleChange={handleChange} />
+                        ))}
+                        <Button variant="contained" color="primary" onClick={handleSubmit} sx={{ mt: 2, ml: 'auto', display: 'block' }}>
+                            Confirmar
+                        </Button>
+                        
+                    </>
+                )}
+            </Box>
+
+            <Modal title="Datos del Formulario" width="400px" open={openModal} setOpen={setOpenModal}>
+                {submittedData && <SubmittedData submittedData={submittedData} />}
+            </Modal>
+        </>
     );
 };
 
